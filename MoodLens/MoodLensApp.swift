e4c19637e644940d5cbb0550ea4933c553ead0e0
Disha Maheshwari <disha.maheshwari@icloud.com>
2025-07-29 14:03:38 -0400

    Initial Commit

---
+++ MoodLens/MoodLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodLensApp.swift
+//  MoodLens
+//
+//  Created by Disha Maheshwari on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
