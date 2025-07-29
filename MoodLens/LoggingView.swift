77d13fe8b0045191b098e7c908d25e54d42df98a
Disha Maheshwari <disha.maheshwari@icloud.com>
2025-07-29 14:18:25 -0400

    created new file for logging

---
+++ MoodLens/LoggingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoggingView.swift
+//  MoodLens
+//
+//  Created by Disha Maheshwari on 7/29/25.
+//
+
+import SwiftUI
+
+struct LoggingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoggingView()
+}
