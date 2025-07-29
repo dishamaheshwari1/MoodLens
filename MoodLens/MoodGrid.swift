97056374d01f177c7c196f8677b83755114de60f
Angelia Causing <aecausing@gmail.com>
2025-07-29 11:13:57 -0700

    created mood grid view

---
+++ MoodLens/MoodGrid.swift
@@ -0,0 +1,18 @@
+//
+//  MoodGrid.swift
+//  MoodLens
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct MoodGrid: View {
+    var body: some View {
+        Text("insert grid here")
+    }
+}
+
+#Preview {
+    MoodGrid()
+}
