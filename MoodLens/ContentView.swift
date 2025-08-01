4bb623bc2550a5c1a905d6d355655b18b6f6645b
Angelia Causing <aecausing@gmail.com>
2025-08-01 12:03:37 -0700

    added apple icon for app, made buttons for home page

--- MoodLens/ContentView.swift
+++ MoodLens/ContentView.swift
@@ -8,141 +8,151 @@
 import SwiftUI
 
 struct ContentView: View {
-   
+    
     
     var body: some View {
         VStack(spacing: 20){
             
             //header
             NavigationStack {
-            VStack(spacing: 16) {
-                
-
+                VStack(spacing: 16) {
                     Text("MOODLENS")
                         .font(.largeTitle)
                         .fontWeight(.bold)
                         .foregroundColor(Color("BlueColorText"))
-               
+                    
                     Text("Your feelings in full color")
                         .font(.subheadline)
                     
-                
                     Divider()
-
-                
-            }
-            .background(Color.accentColor)
+                }
+                .frame(maxWidth: .infinity)
+                .background(
+                    Color.accentColor
+                        .ignoresSafeArea(edges: [.top, .horizontal]))
                 
                 //welcome message
-            Spacer()
-            VStack(alignment: .leading, spacing: 20) {
-                Text("Welcome back, [NAME]!")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                Text("Let's Reflect on Your Day")
-                    .font(.headline)
-                //Mood graph box
-                VStack {
-                    HStack {
-                        Text("Mood Graph Summaries")
-                            .font(.title3)
-                            .fontWeight(.semibold)
-                    Spacer()
-                        NavigationLink(destination: MoodGrid()) {
-                            Text(">")
-                                .font(.title2)
-                                .foregroundColor(Color.blue)
+                Spacer()
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Welcome back, [NAME]!")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Text("Let's Reflect on Your Day")
+                        .font(.headline)
+                    //Mood graph box
+                    VStack {
+                        HStack {
+                            Text("Mood Graph Summaries")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                                .padding(.bottom, -40.0)
+                            Spacer()
+                            NavigationLink(destination: MoodGrid()) {
+                                Text(">")
+                                    .font(.title2)
+                                    .foregroundColor(Color.blue)
+                                    .padding(.bottom, -40.0)
+
                                 
+                            }
                         }
-                
-                        }
-                        Spacer()
                         
                     }
-                    .padding(.horizontal)
-                    .padding(.top)
-                   
-                ZStack{
-                
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color("PastelGreen"))
-                        .frame(height: 200)
-                        .shadow(radius: 3)
+                    .padding(.horizontal, 7)
+                    .padding(.vertical, 25)
                     
-                    HStack(spacing: 60){
+                    
+                    HStack(spacing: 20) {
                         NavigationLink(destination: MoodGrid()) {
                             Text("Exercise")
-                                .foregroundColor(Color.black)
-                                .font(.title2)
-                                .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
-                                .background()
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity, minHeight: 50)
+                                .background(Color("PastelGreen"))
                                 .cornerRadius(10)
-                             
                         }
-                        NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        
+                        NavigationLink(destination: Text("Destination")) {
                             Text("Me-Time")
-                                .font(.title2)
-                                .foregroundColor(Color.black)
-                                .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
-                                .background()
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity, minHeight: 50)
+                                .background(Color("PastelGreen"))
                                 .cornerRadius(10)
+                        }
+                        
+                        NavigationLink(destination: Text("Destination")) {
+                            Text("Sleep")
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity, minHeight: 50)
+                                .background(Color("PastelGreen"))
+                                .cornerRadius(10)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 2)
+
+                    
+                    
+                    //insights section
+                    
+                    VStack(spacing: 8) {
+                        Text("Emotional Insights")
+                            .font(.title3)
+                            .padding(.top, 70.0)
+                            .frame(height: 5.0)
+                            .fontWeight(.semibold)
 
                         
+                        
+                        HStack {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("PastelPink"))
+                                Text("Recognize your own emotional state to see how it affects your daily life")
+                                    .multilineTextAlignment(.center)
+                                    .padding(.horizontal)
+                            }
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("PastelPink"))
+                                Text("Find what keeps you motivated")
+                                    .multilineTextAlignment(.center)
+                            }
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("PastelPink"))
+                                Text("Never undervalue your emotions")
+                                    .multilineTextAlignment(.center)
+                            }
+                            
                         }
+                        .padding(.top, 60)
+                        
+                        
                     }
-                        .padding()
-                }
-                }
-                //insights section
-                
-                VStack(spacing: 8) {
-                    Text("Emotional Insights")
-                        .font(.headline)
-                        .padding(.leading)
+                    Divider()
                     
                     HStack {
-                       
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color("PastelPink"))
-                            Text("Recognize your own emotional state to see how it affects your daily life")
-                                .multilineTextAlignment(.center)
-                                .padding(.horizontal)
-                          }
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color("PastelPink"))
-                            Text("Find what keeps you motivated")
-                                .multilineTextAlignment(.center)
-                        }
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color("PastelPink"))
-                            Text("Never undervalue your emotions")
-                                .multilineTextAlignment(.center)
+                        NavigationLink(destination: LoggingView()) {
+                            Text("Create Log +")
+                                .foregroundColor(Color.blue)
+                                .frame(maxWidth: .infinity, alignment: .center) // Center horizontally
+                            
                         }
                         
                     }
                     
+                    Spacer()
                 }
-                Divider()
-                
-                HStack {
-                    NavigationLink(destination: LoggingView()) {
-                        Text("Create Log +")
-                            .foregroundColor(Color.blue)
-                       
-                        }
-                        
-                    }
-               
                 Spacer()
+                
             }
-            Spacer()
-            
+            .padding()
+            .ignoresSafeArea(edges: .all)
         }
-        .padding()
-        .ignoresSafeArea(edges: .all)    }
+    }
 }
 
 #Preview {
